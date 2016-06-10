f24a15abb25e59f15b6c0b53093a95434e25f350
raphaChoquet <rapha.choquet@gmail.com>
2016-06-10 11:20:16 +0200

    add images for game

--- public/src/app/game/states/preload.js
+++ public/src/app/game/states/preload.js
@@ -9,7 +9,7 @@
                 app.game.load.image('ball', '../img/game/ball.png');
                 app.game.load.image('wall', '../img/game/wall.png');
                 app.game.load.image('end', '../img/game/end.png');
-                app.game.load.image('badGuy', '../img/game/wall.png');
+                app.game.load.image('badGuy', '../img/game/bad-guys.png');
                 app.game.load.image('background', '../img/game/background.png');
             }
 
